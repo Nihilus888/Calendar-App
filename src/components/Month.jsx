044db25170a19e6a8b3d034fe1dc0749cdfced97
Elliott Tay <austonmartin@gmail.com>
2022-12-08 22:31:52 +0800

    added month

--- src/components/Month.jsx
+++ src/components/Month.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
-
-export default function Month() {
+import React from "react";
+import Day from "./Day";
+export default function Month({ month }) {
   return (
-    <div>Month</div>
-  )
+    <div className="flex-1 grid grid-cols-7 grid-rows-5">
+      {month.map((row, i) => (
+        <React.Fragment key={i}>
+          {row.map((day, idx) => (
+            <Day day={day} key={idx} rowIdx={i} />
+          ))}
+        </React.Fragment>
+      ))}
+    </div>
+  );
 }
