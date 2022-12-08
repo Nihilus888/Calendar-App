50b619c463da2fa6bad28ef77bdc005e934e8c86
Elliott Tay <austonmartin@gmail.com>
2022-12-08 23:10:08 +0800

    setting up calendar

--- src/components/Month.jsx
+++ src/components/Month.jsx
@@ -5,11 +5,11 @@ export default function Month({ month }) {
     <div className="flex-1 grid grid-cols-7 grid-rows-5">
       {month.map((row, i) => (
         <React.Fragment key={i}>
-          {row.map((day, idx) => (
-            <Day day={day} key={idx} rowIdx={i} />
-          ))}
+            {row.map((day, idx) => (
+                <Day day={day} key={idx}/>
+            ))}
         </React.Fragment>
       ))}
     </div>
-  );
+  )
 }
