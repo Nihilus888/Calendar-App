b43eb7c9089207eb6418a46b40b57729370d29df
Elliott Tay <austonmartin@gmail.com>
2022-12-09 20:27:10 +0800

    added calendar logo

--- src/components/CalendarHeader.jsx
+++ src/components/CalendarHeader.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import logo from './assets/Calendar.jpg'
 
 export default function CalendarHeader() {
   return (
-    <div></div>
+    <header className="px-4 py-2 flex items-center">
+      <img src={logo} alt="calendar" className="mr-2 w-15 h-12" />
+    </header>
   )
 }
