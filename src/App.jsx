e4ebbfbcca296c6e1d9653d6526e905b28f330e3
Elliott Tay <austonmartin@gmail.com>
2022-12-09 20:42:49 +0800

    added chevron function

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import "./App.css";
 import { getMonth } from "./util";
 import CalendarHeader from "./components/CalendarHeader";
 import Sidebar from "./components/Sidebar";
 import Month from "./components/Month";
+import GlobalContext from "./components/context/GlobalContext";
 
 function App() {
   const [currentMonth, setCurrentMonth] = useState(getMonth());
+  const { monthIndex } = useContext(GlobalContext)
+
+  useEffect(() => {
+    setCurrentMonth(getMonth(monthIndex))
+  }, [monthIndex])
 
   return (
     <React.Fragment>
