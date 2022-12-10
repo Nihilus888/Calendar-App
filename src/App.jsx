b5c9a7c76d9eb0abf587096e7e3cd61196fcb5fa
Elliott Tay <austonmartin@gmail.com>
2022-12-10 18:21:25 +0800

    event modal added into app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import CalendarHeader from "./components/CalendarHeader";
 import Sidebar from "./components/Sidebar";
 import Month from "./components/Month";
 import GlobalContext from "./components/context/GlobalContext";
+import EventModal from "./components/EventModal";
 
 function App() {
   const [currentMonth, setCurrentMonth] = useState(getMonth());
@@ -16,6 +17,7 @@ function App() {
 
   return (
     <React.Fragment>
+      <EventModal />
       <div className="h-screen flex flex-col">
         <CalendarHeader />
         <div className="flex flex-1">
--- src/components/EventModal.jsx
+++ src/components/EventModal.jsx
@@ -1,19 +1,16 @@
-import React from 'react'
+import React from "react";
 
 export default function EventModal() {
   return (
     <div className="h-screen w-full fixed left-0 top-0 flex justify-center items-center">
-        <form className="bg-white rounded-lg shadow-2xl w-1/4">
-            <header className="bg-gray-100 px-4 flex justify-between items-center">
-                <span className="text-gray-400">
-                    drag_handle
-                </span>
-                <button>
-                    
-                </button>
-
-            </header>
-        </form>
+      <form className="bg-white rounded-lg shadow-2xl w-1/4">
+        <header className="bg-gray-100 px-4 flex justify-between items-center">
+          <span className="text-gray-400">drag_handle</span>
+          <button>
+            <span className="text-gray-400">close</span>
+          </button>
+        </header>
+      </form>
     </div>
-  )
+  );
 }
