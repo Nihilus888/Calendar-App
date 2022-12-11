c6d3d6b474da9fe201b90073755ee60541ffd22b
Elliott Tay <austonmartin@gmail.com>
2022-12-11 19:55:54 +0800

    set up useReducer

--- src/components/context/ContextWrapper.jsx
+++ src/components/context/ContextWrapper.jsx
@@ -1,18 +1,38 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useReducer } from "react";
 import GlobalContext from "./GlobalContext";
 import dayjs from "dayjs";
+
+function savedEventsReducer(state, { type, payload }) {
+  switch (type) {
+    case "push":
+      return [...state, payload];
+    case "update":
+      return state.map((evt) => (evt.id === payload.id ? payload : evt));
+    case "delete":
+      return state.filter((evt) => evt.id !== payload.id);
+    default:
+      throw new Error();
+  }
+}
+
+function initEvents() {
+  const storageEvents = localStorage.getItem("savedEvents")
+  const parsedEvents = storageEvents ? JSON.parse(storageEvents) : []
+  return parsedEvents
+}
 
 export default function ContextWrapper(props) {
   const [monthIndex, setMonthIndex] = useState(dayjs().month());
   const [smallCalendarMonth, setSmallCalendarMonth] = useState(null);
-  const [daySelected,  setDaySelected] = useState(dayjs())
+  const [daySelected, setDaySelected] = useState(dayjs());
   const [showEventModal, setShowEventModal] = useState(false);
+  const [savedEvents, dispatchCalEvent] = useReducer(savedEventsReducer, [], initEvents);
 
   useEffect(() => {
-    if(smallCalendarMonth !== null) {
-      setMonthIndex(smallCalendarMonth)
+    if (smallCalendarMonth !== null) {
+      setMonthIndex(smallCalendarMonth);
     }
-  }, [smallCalendarMonth])
+  }, [smallCalendarMonth]);
 
   return (
     <GlobalContext.Provider
