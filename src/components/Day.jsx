4017ea6398680e5d82d697fe90d0e09755b7b395
Elliott Tay <austonmartin@gmail.com>
2022-12-11 21:37:50 +0800

    added save event to the calendar from local storage

--- src/components/Day.jsx
+++ src/components/Day.jsx
@@ -1,13 +1,24 @@
 import dayjs from "dayjs";
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import GlobalContext from "./context/GlobalContext";
 
 export default function Day({ day, rowIdx }) {
-  function getCurrentDayClass() {
-    return day.format("DD--MM-YY") === dayjs().format("DD--MM-YY") ? 'bg-blue-600 text-white rounded-full w-7' : ''
-  }
+  const [dayEvents, setDayEvents] = useState([]);
+  const { setDaySelected, setShowEventModal, savedEvents } =
+    useContext(GlobalContext);
 
-  const {setDaySelected, setShowEventModal} = useContext(GlobalContext)
+  useEffect(() => {
+    const events = savedEvents.filter(
+      (evt) => dayjs(evt.day).format("DD-MM-YY") === day.format("DD-MM-YY")
+    );
+    setDayEvents(events);
+  }, [savedEvents, day]);
+
+  function getCurrentDayClass() {
+    return day.format("DD--MM-YY") === dayjs().format("DD--MM-YY")
+      ? "bg-blue-600 text-white rounded-full w-7"
+      : "";
+  }
 
   return (
     <div className="border border-gray-200 flex flex-col">
@@ -15,13 +26,25 @@ export default function Day({ day, rowIdx }) {
         {rowIdx === 0 && (
           <p className="text-sm mt-1">{day.format("ddd").toUpperCase()}</p>
         )}
-        <p className={`text-sm p-1 my-1 text-center ${getCurrentDayClass()}`}>{day.format("DD")}</p>
+        <p className={`text-sm p-1 my-1 text-center ${getCurrentDayClass()}`}>
+          {day.format("DD")}
+        </p>
       </header>
-      <div className="flex-1 cursor-pointer" onClick={() => {
-        setDaySelected(day)
-        setShowEventModal(true)
-      }}>
-          {""}
+      <div
+        className="flex-1 cursor-pointer"
+        onClick={() => {
+          setDaySelected(day);
+          setShowEventModal(true);
+        }}
+      >
+        {dayEvents.map((evt, idx) => (
+          <div
+            key={idx}
+            className={`bg-${evt.label}-200 p-1 mr-3 text-gray-600 text-sm rounded mb-1 truncate`}
+          >
+            {evt.title}
+          </div>
+        ))}
       </div>
     </div>
   );
