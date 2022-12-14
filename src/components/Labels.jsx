9f47f5c3601be72277a156bfeae707e4d7401c2f
Elliott Tay <austonmartin@gmail.com>
2022-12-14 19:13:12 +0800

    added checkbox for confirm and cancel meetings

--- src/components/Labels.jsx
+++ src/components/Labels.jsx
@@ -12,14 +12,29 @@ export default function Labels() {
           <input
             type="checkbox"
             checked={checked}
-            onChange={() =>
-              updateLabel({ label: lbl, checked: !checked })
-            }
+            onChange={() => updateLabel({ label: lbl, checked: !checked })}
             className={`form-checkbox h-5 w-5 text-${lbl}-400 rounded focus:ring-0 cursor-pointer`}
           />
           <span className="ml-2 text-gray-700 capitalize">{lbl}</span>
         </label>
       ))}
+
+      <p className="text-gray-500 font-bold mt-10">Meeting Confirmed?</p>
+      <p className="mt-3 mb-3">
+        <input
+          type="checkbox"
+          className={`form-checkbox h-5 w-5 text-green-400 rounded focus:ring-0 cursor-pointer`}
+        />
+        <span className="ml-2 text-gray-700 capitalize">Confirmed</span>
+      </p>
+
+      <p>
+        <input
+          type="checkbox"
+          className={`form-checkbox h-5 w-5 text-red-400 rounded focus:ring-0 cursor-pointer`}
+        />
+        <span className="ml-2 text-gray-700 capitalize">Cancelled</span>
+      </p>
     </React.Fragment>
   );
 }
